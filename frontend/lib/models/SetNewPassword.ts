0731276ec8d7a76a9cfe69b6c6dc144bf248aac3
João Pedro <jpedropsss@gmail.com>
2025-06-16 06:51:32 -0400

    Remioção d exportação antiga

--- frontend/lib/models/SetNewPassword.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type SetNewPassword = {
-    password: string;
-    token: string;
-};
-
